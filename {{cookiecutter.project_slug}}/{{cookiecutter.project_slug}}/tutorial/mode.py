349cc88cccbcb0fef089ef52fdddf26013260f96
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2025-07-13 14:11:05 -0700

    Rename tutorial init to preprocess

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tutorial/mode.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tutorial/mode.py
@@ -3,9 +3,9 @@ from sonolus.script.engine import TutorialMode
 from {{ cookiecutter.project_slug }}.lib.effect import Effects
 from {{ cookiecutter.project_slug }}.lib.particle import Particles
 from {{ cookiecutter.project_slug }}.lib.skin import Skin
-from {{ cookiecutter.project_slug }}.tutorial.init import preprocess
 from {{ cookiecutter.project_slug }}.tutorial.instructions import InstructionIcons, Instructions
 from {{ cookiecutter.project_slug }}.tutorial.navigate import navigate
+from {{ cookiecutter.project_slug }}.tutorial.preprocess import preprocess
 from {{ cookiecutter.project_slug }}.tutorial.update import update
 
 tutorial_mode = TutorialMode(
